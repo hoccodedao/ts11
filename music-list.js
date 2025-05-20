a663108547ee79ebe5ba74fd7125d2c5703ab3af
hoccodedao <chillwith01@gmail.com>
2025-05-20 21:41:02 +0700

    First commit

---
+++ music-list.js
@@ -0,0 +1,46 @@
+let allMusic = [
+  {
+    name: "Em gái mưa",
+    artist: "Hương tràm",
+    img: "b1",
+    src: "b1"
+  },
+   {
+    name: "Có tất cả nhưng thiếu anh",
+    artist: "Mr siro",
+    img: "b2",
+    src: "b2"
+  },
+    {
+    name: "Phía sau một gái",
+    artist: "Soobin Hoàng sơn",
+    img: "b3",
+    src: "b3"
+  },
+   {
+    name: "Cẩm tú cầu",
+    artist: "Huỳnh Văn",
+    img: "b4",
+    src: "b4"
+  },
+  {
+    name: "Từng yêu",
+    artist: "Phan Duy Anh",
+    img: "b5",
+    src: "b5"
+  },
+  {
+    name: "Anh chỉ là người thay thế",
+    artist: "Mr siro",
+    img: "b6",
+    src: "b6"
+  },
+   {
+    name: "Đơn phương mình anh",
+    artist: "Linh tuổi thìn",
+    img: "b7",
+    src: "b7"
+  },
+ 
+
+];
